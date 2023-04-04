fec115e44604fc392cbc6fea3e60721b9892fbd7
coincedence <gkrivov12@gmail.com>
2023-04-04 19:45:22 +0300

    [codedrop] Reworked shooting system

--- Assets/Scripts/Game/Weapons/Gun.cs
+++ Assets/Scripts/Game/Weapons/Gun.cs
@@ -1,19 +1,25 @@
 using UnityEngine;
 
 [RequireComponent (typeof(BulletGenerator))]
+
+
 public class Gun : MonoBehaviour
 {
+    [SerializeField] private float _cooldown;
+    private float _lastShotTime;
     private BulletGenerator _generator;
     private void Start()
     {
         _generator = GetComponent<BulletGenerator>();
+        _lastShotTime = Time.time;
     }
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0) && !OnCooldown())
             Shoot();
     }
 
+    private bool OnCooldown() => (Time.time - _lastShotTime)  <= _cooldown;
     private void Shoot()
     {
         var bullet = _generator.TryGetObject();
@@ -23,5 +29,6 @@ public class Gun : MonoBehaviour
         bullet.gameObject.transform.rotation = transform.rotation;
         bullet.Move(transform.up);
         bullet.transform.parent = Camera.main.transform;
+        _lastShotTime = Time.time;
     }
 }
